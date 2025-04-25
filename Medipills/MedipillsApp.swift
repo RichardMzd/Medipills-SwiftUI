47610dbb70a16d4a7a07ba530900d68b71f4cf91
Richard Mazid <Richard.mazid@gmail.com>
2025-04-25 12:24:55 +0200

    Initial Commit

---
+++ Medipills/MedipillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedipillsApp.swift
+//  Medipills
+//
+//  Created by Richard Arif Mazid on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MedipillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
